b7a1472730d7c25e83a7f4629668baf0b035718a
AnnahOkoth <Annah.Okoth@riarauniversity.ac.ke>
2023-05-18 16:36:28 +0300

    hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,11 +4,12 @@ function Hero() {
   return (
     <div className='flex text-green-800 rounded-sm bg-red-100 items-center justify-evenly'>
         <div className='m-4 flex flex-col gap-4'>
-        <h1 className='text-6xl text-bold'>Grab Upto 50% Off On Selected Headphone</h1>
+        <h1 className='text-3xl lg:text-6xl text-bold'>Grab Upto 50% Off On Selected Headphone</h1>
         <button className='bg-green-800 p-3 text-white w-32 rounded-full'>Buy Now</button>
         </div>
-        <div classname='text-cen'>
+        <div >
             <img 
+            className='hidden lg:block'
             src="hero.png"
             />
         </div>
